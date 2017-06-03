1f9ef5752fc6db22fa2b193dca4ded140e4da55d
D-N-D <dimitar.nikolaev.dimitrov@gmail.com>
2017-06-04 01:53:15 +0300

    Ex1

---
+++ Post/Post.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace Post
+{
+    public class Post
+    {
+        private string _title;
+        private string _description;
+        private DateTime _created;
+        private int _vote;
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public DateTime Created
+        {
+            get { return _created; }
+            set { _created = value; }
+        }
+
+        public int Vote
+        {
+            get { return _vote; }
+        }
+
+        public void UpVote()
+        {
+            _vote = _vote + 1;
+        }
+
+        public void DownVote ()
+        {
+            _vote = _vote - 1;
+        }
+
+        public Post()
+        {
+
+		 _vote = 0;
+
+	    }
+
+    }
+}
---
+++ Post/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Post
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var post = new Post();
+            Console.WriteLine(post.Title+"|");
+
+            post.Title = "Title1";
+            Console.WriteLine(post.Title);
+            Console.WriteLine(post.Vote);
+
+            post.UpVote();
+            post.UpVote();
+
+           // post.Vote = 5;
+
+            Console.WriteLine(post.Vote);
+
+            post.DownVote();
+
+			Console.WriteLine(post.Vote);
+		}
+    }
+}
